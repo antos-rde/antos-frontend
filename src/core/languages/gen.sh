4290ea602abe224fa8efb571921539c27b321d37
Xuan Sang LE <xsang.le@gmail.com>
2018-03-17 23:46:37 +0100

    minor fix

--- src/core/languages/gen.sh
+++ src/core/languages/gen.sh
@@ -29,9 +29,10 @@ grep --include=\*.{coffee,tag} -roh "$1" -e '__("[^"]*"' | while read -r line ; 
         echo -e "\t\"$SUBSTRING\":\"$SUBSTRING\"," >> "tmp.json"
     fi
 done
-grep --include=\*.{coffee,html,tag} -roh "$1" -e '\"__\([^\"]*\)\"' | while read -r line; do
+grep --include=\*.{coffee,html,tag} -roh "$1" -e '\"__([^\"]*)\"' | while read -r line; do
     len=$(( ${#line} - 6 ))
     #echo $len
+    #echo $line
     SUBSTRING=${line:4:len}
     #echo $SUBSTRING
     if  test -f "$2"   && [ ! -z "$(grep  -F "\"$SUBSTRING\":" "$2")" ]
@@ -41,21 +42,26 @@ grep --include=\*.{coffee,html,tag} -roh "$1" -e '\"__\([^\"]*\)\"' | while read
         echo -e "\t\"$SUBSTRING\":\"$SUBSTRING\"," >> "tmp.json"
     fi
 done
-sort tmp.json > tmp1.json
-awk '!a[$0]++' "tmp1.json" > tmp.json
-sed '$ s/.$//' tmp.json > tmp1.json
-# remove duplicate entry
-if test -f $2
+if test -f tmp.json
 then
-    cp $2 "$2.old"
-    sed '$ s/.$//' $2 > tmp.json
-    cat tmp.json > $2
-    echo "," >> $2
-    cat tmp1.json >> $2
-    echo "}" >> "$2"
+    sort tmp.json > tmp1.json
+    awk '!a[$0]++' "tmp1.json" > tmp.json
+    sed '$ s/.$//' tmp.json > tmp1.json
+    # remove duplicate entry
+    if test -f $2
+    then
+        cp $2 "$2.old"
+        sed '$ s/.$//' $2 > tmp.json
+        cat tmp.json > $2
+        echo "," >> $2
+        cat tmp1.json >> $2
+        echo "}" >> "$2"
+    else
+        echo "{"> "$2"
+        cat tmp1.json >> "$2"
+        echo "}" >> "$2"
+    fi
+    rm tmp.json tmp1.json
 else
-     echo "{"> "$2"
-    cat tmp1.json >> "$2"
-    echo "}" >> "$2"
+    echo "Nothing change"
 fi
-rm tmp.json tmp1.json
